9b95204934ea94ad255a1befe47070de64b5a3f6
aefuente <aefuente@pm.me>
2025-12-13 10:43:11 -0600

    move parsing to its own function so we can write test easier

--- src/argparser.zig
+++ src/argparser.zig
@@ -10,14 +10,27 @@ pub const Args = struct {
     issue_type: issueType,
 
     pub fn init(allocator: Allocator) !Args {
-        var stdout_buf: [1024]u8 = undefined;
-        var stdout = std.fs.File.stdout().writer(&stdout_buf);
-
         const command_args = try std.process.argsAlloc(allocator);
         errdefer process.argsFree(allocator, command_args);
 
+        var args = Args{
+            .arg_buf = command_args,
+            .action = undefined,
+            .target = undefined,
+            .description = undefined,
+            .issue_type = undefined,
+        };
 
-        if (std.mem.eql(u8, command_args[1], "--help") or std.mem.eql(u8, command_args[1], "-h")) {
+        try args.parse();
+
+        return args;
+    }
+
+    fn parse(self: *Args) !void {
+        var stdout_buf: [1024]u8 = undefined;
+        var stdout = std.fs.File.stdout().writer(&stdout_buf);
+
+        if (self.arg_buf.len > 1 and (std.mem.eql(u8, self.arg_buf[1], "--help") or std.mem.eql(u8, self.arg_buf[1], "-h"))) {
             try stdout.interface.print("clerk -- an issue tracker\n\n", .{});
             try stdout.interface.print("Usage:\n\tclerk <action>\n", .{});
             try stdout.interface.print("\tclerk <action> <target> [options]\n", .{});
@@ -41,32 +54,24 @@ pub const Args = struct {
             return error.Help;
         }
 
-        if (command_args.len < 3) {
+        if (self.arg_buf.len < 3) {
             return error.TooFewArguements;
         }
 
-        var arg = Args{
-            .arg_buf = command_args,
-            .action = try getAction(command_args[1]),
-            .target = command_args[2][0..command_args[2].len],
-            .description = undefined,
-            .issue_type = undefined,
-        };
-
+        self.action = try getAction(self.arg_buf[1]);
+        self.target = self.arg_buf[2][0..self.arg_buf[2].len];
         
         var idx: usize = 3;
 
-        while (idx+1 < command_args.len) : (idx += 2) {
-            if (!isVariable(command_args[idx])){
+        while (idx+1 < self.arg_buf.len) : (idx += 2) {
+            if (!isVariable(self.arg_buf[idx])){
                 return error.ExpectedVar;
             }
-            if (isVariable(command_args[idx+1])) {
+            if (isVariable(self.arg_buf[idx+1])) {
                 return error.ExpectedValue;
             }
-            try setVariable(&arg, command_args[idx], command_args[idx+1]);
+            try setVariable(self, self.arg_buf[idx], self.arg_buf[idx+1]);
         }
-
-        return arg;
     }
 
     pub fn deinit(self: *Args, allocator: Allocator) void {
