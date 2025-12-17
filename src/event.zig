60b3feb273eb35e641816b9fc2bb880eae2c98d4
aefuente <aefuente@pm.me>
2025-12-17 12:29:42 -0600

    adding search results to state

--- src/event.zig
+++ src/event.zig
@@ -20,8 +20,8 @@ pub fn run(allocator: Allocator) !void {
     var stdout_buf: [1024]u8 = undefined;
     var stdout = std.fs.File.stdout().writer(&stdout_buf);
 
-    var display = try screen.init(&stdout.interface);
-    defer display.deinit();
+    var display = try screen.init(allocator, &stdout.interface);
+    defer display.deinit(allocator);
     try display.DrawBoxes();
     try display.userInteraction(allocator);
 
@@ -37,13 +37,13 @@ pub const screen = struct {
     selection_pos: usize,
     clerk: issue.Clerk,
     terminal: terminal.Terminal,
-    result_len: usize,
+    search_result: []issue.Issue,
 
-    pub fn init(stdout: *std.Io.Writer) !screen {
+    pub fn init(allocator: Allocator, stdout: *std.Io.Writer) !screen {
         const term_size = try render.getTerminalSize();
         const result = render.CalculateResult(term_size);
         const preview = render.CalculatePreview(term_size);
-        const search = render.Box{
+        const s_box = render.Box{
             .identifier = "Search",
             .x = result.x,
             .y = result.y + result.height + 1,
@@ -52,22 +52,22 @@ pub const screen = struct {
         };
 
         const search_pos = render.SearchDetails{
-            .x = search.x + 2,
-            .y = search.y + 1,
-            .width = search.width-2
+            .x = s_box.x + 2,
+            .y = s_box.y + 1,
+            .width = s_box.width-2
         };
 
         return screen{
             .stdout = stdout,
             .result_box = result,
             .preview_box = preview,
-            .search_box = search,
+            .search_box = s_box,
             .search_bounds = search_pos,
             .cursor_pos = 0,
             .selection_pos = 0,
             .clerk = try issue.Clerk.init(),
             .terminal = try terminal.Terminal.init(),
-            .result_len = 0,
+            .search_result = try allocator.alloc(issue.Issue, 0),
         };
     }
 
@@ -100,7 +100,7 @@ pub const screen = struct {
         const issues = try self.clerk.getIssueList(allocator);
         defer allocator.free(issues);
 
-        self.populate_search(allocator, array_list.items, issues);
+        try self.populateSearch(allocator, array_list.items, issues);
 
         try self.stdout.print("\x1b[{};{}H", .{self.search_bounds.y, self.search_bounds.x});
         try self.stdout.flush();
@@ -119,9 +119,10 @@ pub const screen = struct {
                     continue;
                 }
 
+                self.selection_pos = 0;
                 self.cursor_pos -= 1;
                 _ = array_list.orderedRemove(self.cursor_pos);
-                self.populate_search(allocator, array_list.items, issues);
+                try self.populateSearch(allocator, array_list.items, issues);
                 try draw_line(self.stdout, array_list.items, self.cursor_pos, self.search_bounds);
             }
 
@@ -161,15 +162,15 @@ pub const screen = struct {
                             continue;
                         },
                         UP_ARROW => {
-                            if (self.selection_pos >= self.result_len-1) { continue; }
+                            if (self.selection_pos+1 >= self.search_result.len) { continue; }
                             self.selection_pos += 1;
-                            self.populate_search(allocator, array_list.items, issues);
+                            try self.populateSearch(allocator, array_list.items, issues);
                             try draw_line(self.stdout, array_list.items, self.cursor_pos, self.search_bounds);
                         },
                         DOWN_ARROW => {
                             if (self.selection_pos > 0) {
                                 self.selection_pos -= 1;
-                                self.populate_search(allocator, array_list.items, issues);
+                                try self.populateSearch(allocator, array_list.items, issues);
                                 try draw_line(self.stdout, array_list.items, self.cursor_pos, self.search_bounds);
                             }
                         },
@@ -180,68 +181,54 @@ pub const screen = struct {
             else {
                 try array_list.insert(allocator, self.cursor_pos, c);
                 self.cursor_pos +=1;
-                self.populate_search(allocator, array_list.items, issues);
+                self.selection_pos = 0;
+                try self.populateSearch(allocator, array_list.items, issues);
                 try draw_line(self.stdout, array_list.items, self.cursor_pos, self.search_bounds);
             }
         }
     }
 
-    pub fn populate_search(self: *screen, allocator: Allocator, query: []const u8, issues: []issue.Issue) void {
+    pub fn populateSearch(self: *screen, allocator: Allocator, query: []const u8, issues: []issue.Issue) !void {
+        try self.search(allocator, query, issues);
+        self.print_search();
+    }
+
+    pub fn search(self: *screen, allocator: Allocator, query: []const u8, issues: []issue.Issue) !void{
+        allocator.free(self.search_result);
+        if (query.len == 0) {
+            self.search_result = try allocator.alloc(issue.Issue, issues.len);
+            @memcpy(self.search_result, issues);
+        }else {
+            self.search_result = try fuzzy.filterAndSort(allocator, query, issues, 30);
+        }
+    }
+
+    pub fn print_search(self: *screen) void {
         var cur_row = self.result_box.y + self.result_box.height - 1;
         const col = self.result_box.x + 2;
-
         var clear = self.result_box.y + 1;
+
         while (clear < self.result_box.y + self.result_box.height) : (clear += 1) {
             self.stdout.print("\x1b[{};{}H", .{clear, col}) catch {};
             for (0..self.result_box.width-1) |_| self.stdout.print(" ", .{}) catch {};
         }
-        if (query.len == 0) {
-            for (issues, 0..) |is, idx| {
-                if (idx == self.selection_pos) {
-                    self.stdout.print("\x1b[{};{}H\x1b[30;43m{s}\x1b[0m", .{cur_row, col, is.title}) catch {};
-                }else {
-                    self.stdout.print("\x1b[{};{}H{s}", .{cur_row, col, is.title}) catch {};
-                }
-
-                if (cur_row <= self.result_box.y) {
-                    break;
-                }
-                cur_row -= 1;
-            }
-
-            if (issues.len < self.result_box.height - self.result_box.y) {
-                self.result_len = issues.len;
+        for (self.search_result, 0..) |is, idx| {
+            if (idx == self.selection_pos) {
+                self.stdout.print("\x1b[{};{}H\x1b[30;43m{s}\x1b[0m", .{cur_row, col, is.title}) catch {};
             }else {
-                self.result_len = self.result_box.height - self.result_box.y;
+                self.stdout.print("\x1b[{};{}H{s}", .{cur_row, col, is.title}) catch {};
             }
-        }else {
-            const se = fuzzy.filterAndSort(allocator, query, issues, 30) catch { return;};
-            for (se, 0..) |is, idx| {
-                if (idx == self.selection_pos) {
-                    self.stdout.print("\x1b[{};{}H\x1b[30;43m{s}\x1b[0m", .{cur_row, col, is.title}) catch {};
-                }else {
-                    self.stdout.print("\x1b[{};{}H{s}", .{cur_row, col, is.title}) catch {};
-                }
-                if (cur_row <= self.result_box.y) {
-                    break;
-                }
-                cur_row -= 1;
-            }
-
-            if (se.len < self.result_box.height - self.result_box.y) {
-                self.result_len = se.len;
-            }else {
-                self.result_len = self.result_box.height - self.result_box.y;
-            }
+            cur_row -= 1;
         }
 
         self.stdout.flush() catch {};
 
     }
 
-    pub fn deinit(self: *screen) void {
+    pub fn deinit(self: *screen, allocator: Allocator) void {
         self.clerk.deinit();
         self.terminal.close();
+        allocator.free(self.search_result);
     }
 
 };
