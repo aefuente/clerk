e8741912a012a5106c038905b5c262921affd339
Andre Fuentes <aefuente@pm.me>
2026-01-11 09:32:08 -0600

    properly render full words in description

--- src/event.zig
+++ src/event.zig
@@ -356,29 +356,28 @@ pub const screen = struct {
             cur_row = 5;
         }
 
+
         if (is.description) |d| {
-
-            var idx: usize = 0;
-            var cur_col: usize = col_start;
-
             try self.stdout.print("\x1b[{};{}H", .{row_start+cur_row, col_start});
-            while (idx < d.len) {
-                if (cur_col >= col_start + max_width) { 
-                    cur_col = col_start;
-                    cur_row += 1;
-                    try self.stdout.print("\x1b[{};{}H", .{row_start+cur_row, cur_col});
+            var cur_col: usize = col_start;
+            const max_col = col_start + max_width;
+            const max_row = self.preview_box.y + self.preview_box.height;
+
+            var split_it = std.mem.splitAny(u8, d, " \n");
+            while (split_it.next()) | val | {
+                if (cur_row + row_start >= max_row - 1 ) {
+                    break;
                 }
-                if (cur_row >= max_height + row_start) {break;}
-                if (d[idx] == '\n') {
-                    cur_col = col_start;
+
+                if (cur_col + val.len >= max_col) {
                     cur_row += 1;
-                    try self.stdout.print("\x1b[{};{}H", .{row_start+cur_row, cur_col});
-                }else {
-                    try self.stdout.print("{c}", .{d[idx]});
+                    try self.stdout.print("\x1b[{};{}H", .{row_start+cur_row, col_start});
+                    cur_col = col_start;
                 }
-                idx += 1;
-                cur_col +=1;
+                try self.stdout.print("{s} ", .{val});
+                cur_col += val.len + 1;
             }
+
         }
         try self.stdout.flush();
     }
